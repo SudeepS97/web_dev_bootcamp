1114271eba796e25888386ba207f9457f209e298
Sudeep Swarnapuri <sudeepswarnapuri@gmail.com>
2023-02-06 12:42:02 -0600

    Section 32: Authentcation & Security - passport

--- _testing/secrets/app.js
+++ _testing/secrets/app.js
@@ -3,8 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { MongoClient } = require('mongodb');
+const session = require("express-session")
+const passport = require("passport")
+const pasportLocal = require("passport-local")
 const db_conn = require(__dirname + "/db_conn.js");
 const bcrypt = require("bcryptjs")
+// const md5 = require("md5")
+const saltRounds = 10;
 
 const hashPassword = async (password) => {
     const saltedPassword = password + pepper;
@@ -19,12 +24,19 @@ const comparePassword = async (password, hash) => {
 
 const app = express();
 const port = process.env.PORT || 3000;
-const saltRounds = 10;
 const pepper = process.env.PASSWORD_PEPPER;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(session({
+    secret: "This is my secret string",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
+app.use(passport.initialize());
+app.use(passport.session())
 
 const uri = process.env.MONGO_URI
 const client = new MongoClient(uri);
@@ -44,13 +56,23 @@ app.get('/register', (req, res) => {
     res.render('register');
 });
 
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login")
+    }
+})
+
 app.post("/login", async function (req, res) {
     const creds = {
         email: req.body.username
     };
     const returnedUsers = await db_conn.findItemsByCreds(client, db_name, collection_name, creds)
-    if (returnedUsers) {
+    console.log(returnedUsers);
+    if (returnedUsers.length > 0) {
         const match = await comparePassword(req.body.password, returnedUsers[0].password);
+        // const match = md5(req.body.password) == returnedUsers[0].password
         if (match) {
             res.render("secrets");
             console.log("Login successful.");
@@ -65,13 +87,17 @@ app.post("/login", async function (req, res) {
 });
 
 app.post("/register", async function (req, res) {
+
     const hash = await hashPassword(req.body.password);
+    // const hash = md5(req.body.password);
     const newUser = {
         email: req.body.username,
         password: hash
     };
     const result = await db_conn.createItem(client, db_name, collection_name, newUser);
-    res.render("secrets")
+    passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+    })
 })
 
 app.listen(port, () => console.log(`Server started at port: ${port}`));
