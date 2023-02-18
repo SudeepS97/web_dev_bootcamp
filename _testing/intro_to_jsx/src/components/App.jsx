3f8bf258e6112c84d440372d71bc1eac59db00df
Sudeep Swarnapuri <sudeepswarnapuri@gmail.com>
2023-02-17 23:51:04 -0600

    Section 33: React - L401

---
+++ _testing/intro_to_jsx/src/components/App.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Heading from "./components/Heading"
+import List from "./components/List"
+
+const name = "Sudeep"
+const time = new Date().getHours()
+const currYear = new Date().getFullYear()
+
+let ToD;
+const customStyle = {
+    color: ""
+}
+
+if (time < 12) {
+    ToD = "Morning"
+    customStyle.color = 'red'
+} else if (time < 18) {
+    ToD = "Afternoon"
+    customStyle.color = 'green'
+} else {
+    ToD = "Night"
+    customStyle.color = 'blue'
+}
+
+function App() {
+    return (<div>
+        <Heading />
+        <h1 className="heading" style={customStyle}>Good {ToD}!</h1>
+        <br></br>
+        <List />
+        <br></br>
+        <p contentEditable='true' spellCheck='false'>Created by {name}</p>
+        <p>Copyright {currYear}</p>
+    </div>,
+        document.getElementById("root"))
+};
+
+export default App
---
+++ _testing/intro_to_jsx/src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function List() {
+    return <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+    </ul>
+}
+
+export default List
