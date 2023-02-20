0894de8982d11a4a066cf617ad1eac292b6b7d3d
Sudeep Swarnapuri <sudeepswarnapuri@gmail.com>
2023-02-19 22:20:44 -0600

    update imports

--- _testing/intro_to_jsx/src/index.js
+++ _testing/intro_to_jsx/src/index.js
@@ -1,37 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Heading from "./components/Heading"
-import List from "./components/List"
-
-const name = "Sudeep"
-const time = new Date().getHours()
-const currYear = new Date().getFullYear()
-
-let ToD;
-const customStyle = {
-    color: ""
-}
-
-if (time < 12) {
-    ToD = "Morning"
-    customStyle.color = 'red'
-} else if (time < 18) {
-    ToD = "Afternoon"
-    customStyle.color = 'green'
-} else {
-    ToD = "Night"
-    customStyle.color = 'blue'
-}
+import App from "./components/App"
 
 ReactDOM.render(
-    <div>
-        <Heading />
-        <h1 className="heading" style={customStyle}>Good {ToD}!</h1>
-        <br></br>
-        <List />
-        <br></br>
-        <p contentEditable='true' spellCheck='false'>Created by {name}</p>
-        <p>Copyright {currYear}</p>
-    </div>,
+    <App />,
     document.getElementById("root")
 );
